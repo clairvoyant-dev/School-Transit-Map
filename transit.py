6559613d01410973343a131be2aa80d8218502a2
clairvoyant-dev <sagar.phirke@gmail.com>
2026-09-07 16:20:34 -0700

    Create transit.py

---
+++ transit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.title("School & Transit Analysis")
+
+# Read our data
+locations = pd.read_csv("locations.csv")
+distance = pd.read_csv("distance.csv")
+
+# Show the chart
+st.header("Distance to Nearest Transit Station")
+
+st.bar_chart(
+    distance,
+    x=distance.columns[0],
+    y=distance.columns[1],
+    horizontal=True
+)
+
+# Show the map
+st.header("School and Transit Locations")
+
+st.map(
+    locations,
+    latitude="Latitude",
+    longitude="Longitude"
+)
