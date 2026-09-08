9c826adf9fb09b61a55b2d40d9cc6f381eedabb0
clairvoyant-dev <sagar.phirke@gmail.com>
2026-09-07 17:25:24 -0700

    Update transit.py

--- transit.py
+++ transit.py
@@ -1,15 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("School & Transit Analysis")
+#st.title("School & Transit Analysis")
 
 # Read our data
 distance = pd.read_csv("Distance.csv")
 
 far_schools = pd.read_csv("Locations.csv")
-
-# Show the chart
-st.header("Distance to Nearest Transit Station")
 
 #st.bar_chart(
 #   distance,
@@ -19,12 +16,14 @@ st.header("Distance to Nearest Transit Station")
 #)
 
 # Show the map
-st.header("School and Transit Locations")
+#st.header("School and Transit Locations")
 
 import streamlit as st
 
 st.title("Congressional District 35: Public Transit & Schools Analysis")
-st.write("This app explores public transit accessibility for local schools in our district.")
+st.write("This analysis explores public transit accessibility for local schools in our district.")
+df = pd.read_csv("Avg of Distance (miles) by School Name.csv")
+st.dataframe(df, use_container_width=True)
 
 st.header("1. Overview: Distance of Transit Stations by School")
 st.image("Distance of Transit Station by School Name.jpg", caption="Distance of Transit Station by School Name")
